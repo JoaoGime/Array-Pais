b2e31a62db635a421500344a6ea2ca4197ab7a1c
João Gimenes <joaopedrossgg@gmail.com>
2025-04-07 10:36:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+var pais = ["Holanda", "Inglatera" , "África do Sul" , "Chile" , "Argentina" , "Brasil" , "Uruguai" , "Colômbia" , "Canadá" ,"EUA" , "Peru" , "Angola" , "Montenegro" , "Egito" , "Haiti"] 
+
+var paisEscolhido = parseInt(prompt("Digite um número de 0 a 15"))
+
+
+document.write("País Escolhido: " + pais[paisEscolhido])
